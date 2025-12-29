57543ad18f921a5593ce19f910f615fdf96d6516
JHGit-hub <julien.hardy2104@gmail.com>
2025-12-29 14:06:29 +0100

    nav footer mobil - maj

--- src/components/FooterNavMobil.jsx
+++ src/components/FooterNavMobil.jsx
@@ -1,8 +1,8 @@
 export default function FooterNavMobile() {
 
     return (
-        <nav className="footer-nav-mobile" aria-label="Navigation mobile principale">
-            <ul>
+        <nav className="footer-nav-mobile fixed bottom-[0px] left-[0px] w-full z-60" aria-label="Navigation mobile principale">
+            <ul className="flex justify-around items-center h-[56px]">
                 <li><a href="#about" title="Aller à la section À propos de moi">À propos</a></li>
                 <li><a href="#skills" title="Aller à la section Mes compétences">Compétences</a></li>
                 <li><a href="#projects" title="Aller à la section Mes projets">Projets</a></li>
